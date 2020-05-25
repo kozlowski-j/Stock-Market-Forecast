bb7ed321784c55a8b8f2175b8143bb97bf64d852
kozlowski-j <jp_kozlowski@outlook.com>
2020-05-25 16:38:11 +0200

    Prepare one dataflow for training RNN

--- utils_tensorflow.py
+++ utils_tensorflow.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 
 def create_time_steps(length):
@@ -21,27 +22,6 @@ def univariate_data(dataset, start_index, end_index, history_size, target_size):
         labels.append(dataset[i + target_size])
 
     return np.array(data), np.array(labels)
-
-
-def prepare_dates(dataset, start_index, end_index, history_size, target_size):
-    history_dates = []
-    future_dates = []
-
-    start_index = start_index + history_size
-    if end_index is None:
-        end_index = len(dataset) - target_size
-
-    for i in range(start_index, end_index):
-        indices = range(i - history_size, i)
-        # Reshape data from (history_size,) to (history_size, 1)
-        history_dates.append(np.reshape(dataset[indices], (history_size, 1)))
-
-        if target_size == 1:
-            future_dates.append(dataset[i + target_size])
-        else:
-            future_dates.append(dataset[i: i + target_size])
-
-    return np.array(history_dates), np.array(future_dates)
 
 
 def show_plot(plot_data, delta, title):
@@ -89,8 +69,8 @@ def show_plot_dates(time_steps, plot_data, delta, title):
     return plt
 
 
-def multivariate_data(dataset, target, start_index, end_index, history_size,
-                      target_size, single_step=False):
+def prepare_batches(dataset, target, start_index, end_index, history_size,
+                    target_size, batch_size=64, buffer_size=1000):
     data = []
     labels = []
 
@@ -102,12 +82,59 @@ def multivariate_data(dataset, target, start_index, end_index, history_size,
         indices = range(i - history_size, i)
         data.append(dataset[indices])
 
-        if single_step:
+        if target_size == 1:
             labels.append(target[i+target_size])
         else:
             labels.append(target[i: i+target_size])
 
-    return np.array(data), np.array(labels)
+    data = np.array(data)
+    labels = np.array(labels)
+
+    # Split data into tensors, according to the tutorial
+    # Buffer and batch sizes acc. to the tutorial as well
+    data_tensors = tf.data.Dataset.from_tensor_slices((data, labels))
+    batched_tensors = data_tensors.cache().shuffle(buffer_size).batch(batch_size).repeat()
+
+    return batched_tensors
+
+
+def prepare_dates(dataset, start_index, end_index, history_size, target_size, batch_size=64):
+    """
+    This functions prepares date indexes for prediction and plotting.
+
+    Args:
+        dataset:
+        start_index:
+        end_index:
+        history_size:
+        target_size:
+        batch_size:
+
+    Returns:
+
+    """
+    history_dates = []
+    future_dates = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i - history_size, i)
+
+        # Reshape data from (history_size,) to (history_size, 1)
+        history_dates.append(np.reshape(dataset[indices], (history_size, 1)))
+
+        if target_size == 1:
+            future_dates.append(dataset[i + target_size])
+        else:
+            future_dates.append(dataset[i: i + target_size])
+
+    dates_tensors = tf.data.Dataset.from_tensor_slices((history_dates, future_dates))
+    batched_dates = dates_tensors.batch(batch_size).repeat()
+
+    return batched_dates
 
 
 def plot_train_history(history, title):
@@ -141,15 +168,13 @@ def multi_step_plot(history, true_future, prediction):
     plt.show()
 
 
-def multi_step_plot_dates(time_index, history, true_future, prediction):
-    #     num_in = create_time_steps(len(history))
-    num_out = len(true_future)
+def multi_step_plot_dates(x_dates, history, y_dates, true_future, prediction=None):
 
-    plt.plot(time_index.flatten(), history[:, 1], label='History')
-    plt.plot(np.arange(num_out), true_future, 'bo',
+    plt.plot(x_dates.flatten(), history[:, 1], label='History')
+    plt.plot(y_dates.flatten(), true_future, 'b-o',
              label='True Future')
-    if prediction.any():
-        plt.plot(np.arange(num_out), prediction, 'ro',
+    if prediction:
+        plt.plot(y_dates.flatten(), prediction, 'r-o',
                  label='Predicted Future')
     plt.legend(loc='upper left')
     plt.xticks(rotation=30)
